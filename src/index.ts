29002a388180d91145a14cea9a0b9e7d370fce75
Nicholas Horsman <nicholas.horsman@gmail.com>
2023-04-07 00:39:47 -0400

    feat: allow prettier to recognize plugin
    Added the exported values necessary for prettier to recognize this as a valid plugin module.
    This is the barest of bare-bones implementations.
    The plugin will overwrite any text in an HTB file with an empty string.
    
    Actual parsing will be added at a later date.

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,77 @@
-export const main = (): string => "Hello World"
+import type {
+  AST,
+  AstPath,
+  Doc,
+  Parser,
+  ParserOptions,
+  Plugin,
+  Printer,
+  RequiredOptions,
+  SupportLanguage,
+  SupportOptions,
+} from 'prettier';
 
-console.log(main())
+export const plugin: Plugin = {
+  languages: [
+    {
+      name: 'HT Basic',
+      parsers: ['htbasic'],
+      extensions: ['.htb', '.stp'],
+      vscodeLanguageIds: ['htbasic'],
+    },
+  ],
+  parsers: {
+    htbasic: {
+      parse(
+        text: string,
+        parsers: { [parserName: string]: Parser },
+        options: ParserOptions
+      ): AST {
+        return { comments: [] };
+      },
+
+      astFormat: 'htbasic-ast',
+
+      locStart(node: any): number {
+        return 0;
+      },
+
+      locEnd(node: any): number {
+        return 0;
+      },
+
+      hasPragma(text: string): boolean {
+        return false;
+      },
+
+      preprocess(text: string, options: ParserOptions): string {
+        return text;
+      },
+    },
+  },
+  printers: {
+    'htbasic-ast': {
+      print(
+        path: AstPath,
+        options: ParserOptions,
+        print: (path: AstPath) => Doc
+      ): Doc {
+        return '';
+      },
+    },
+  },
+  options: {},
+  defaultOptions: {
+    printWidth: Number.MAX_SAFE_INTEGER,
+    proseWrap: 'never',
+  },
+};
+
+export const languages: SupportLanguage[] | undefined = plugin.languages;
+export const parsers: { [parserName: string]: Parser } | undefined =
+  plugin.parsers;
+export const printers: { [astFormat: string]: Printer } | undefined =
+  plugin.printers;
+export const options: SupportOptions | undefined = plugin.options;
+export const defaultOptions: Partial<RequiredOptions> | undefined =
+  plugin.defaultOptions;
